2cfe244fd59bcdd4cc45746e1c3fe087628981c0
Ryan Weiler <ryanweiler92@gmail.com>
2024-12-13 22:16:18 +0000

    updating model type env variable name

--- deployer/deployment_mixin.py
+++ deployer/deployment_mixin.py
@@ -15,7 +15,7 @@ class DeploymentMixin:
             env=[
                 client.V1EnvVar(name="MODEL", value=self.model_name),
                 client.V1EnvVar(name="MODEL_REPO_ID", value=self.model_repo_id),
-                client.V1EnvVar(name="TYPE", value=self.model_type),
+                client.V1EnvVar(name="MODEL_TYPE", value=self.model_type),
             ],
             ports=[client.V1ContainerPort(container_port=8888)],
             volume_mounts=[
