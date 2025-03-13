2307662de60603d3b3891744b118704ea57484f4
Ryan Weiler <ryanweiler92@gmail.com>
2025-03-13 21:23:46 +0000

    update stop route

--- router/stop_route.py
+++ router/stop_route.py
@@ -32,7 +32,7 @@ async def stop_model(model_id: str = None, model: str = None):
         deployer.delete_podmonitoring()
         deployer.delete_hpa()
         deployer.unregister_active_model()
-        await deployer.delete_deployment_status()
+        # await deployer.delete_deployment_status()
     except Exception as e:
         logger.error(f"Error stopping model {model}/{model_id}: {e}")
         raise HTTPException(
